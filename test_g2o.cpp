f7c5d09a69adde66d0c615f339b4bc1545d0b0c6
jingdao <chenjingdao@wustl.edu>
2015-12-02 15:55:27 -0500

    added jacobian calculation

--- test_g2o.cpp
+++ test_g2o.cpp
@@ -15,6 +15,7 @@
 #include "g2o/core/base_vertex.h"
 #include "g2o/core/base_unary_edge.h"
 #include "g2o/solvers/dense/linear_solver_dense.h"
+#include "g2o/core/jacobian_workspace.h"
 //#include "g2o/solvers/csparse/linear_solver_csparse.h"
 
 //http://dspguru.com/dsp/howtos/how-to-generate-white-gaussian-noise
@@ -70,42 +71,71 @@ class EdgeLine : public g2o::BaseUnaryEdge<1, Eigen::Vector2d, VertexLine> {
 		const double& y = measurement()(1);
 		_error(0) = y - (m*x + c);
 	}
+
+	void linearizeOplus() {
+		_jacobianOplusXi(0,0) = - measurement()(0);
+		_jacobianOplusXi(0,1) = -1;
+	}
 };
 
-int main() {
-	//initialize solver
-	g2o::SparseOptimizer optimizer;
-	g2o::BlockSolverX::LinearSolverType *linearSolver = new g2o::LinearSolverDense<g2o::BlockSolverX::PoseMatrixType>();
-	g2o::BlockSolverX *blocksolver = new g2o::BlockSolverX(linearSolver);
-	g2o::OptimizationAlgorithmLevenberg* solver = new g2o::OptimizationAlgorithmLevenberg(blocksolver);
-	optimizer.setAlgorithm(solver);
+int main(int argc,char* argv[]) {
 	srand(time(NULL));
+	int numTrials = 100;
+	double noise_sigma = 5;
+	int num_edges = 100;
 
-	//add vertex
-	VertexLine *v = new VertexLine();
-	v->setId(0);
-	v->setEstimate(Eigen::Vector2d(0.0,2.0));
-	optimizer.addVertex(v);
-	//add edges
-	for (int i=0;i<100;i++) {
-		double x = i;
-		double y = 2 * x + 3 + randn();
-		EdgeLine* e = new EdgeLine();
-		e->setInformation(Eigen::Matrix<double,1,1>::Identity());
-		e->setVertex(0,v);
-		e->setMeasurement(Eigen::Vector2d(x,y));
-		optimizer.addEdge(e);
+	for (int i=1;i<argc-1;i++) {
+		if (strcmp(argv[i],"-n") == 0) {
+			numTrials = atoi(argv[i+1]);
+			i++;
+		} else if (strcmp(argv[i],"-g") == 0) {
+			num_edges = atoi(argv[i+1]);
+			i++; 
+		} else if (strcmp(argv[i],"-s") == 0) {
+			noise_sigma = atof(argv[i+1]);
+			i++;
+		}
 	}
 
-	//optimize
-	int nIter = 10;
-	optimizer.initializeOptimization();
-	optimizer.setVerbose(true);
-	optimizer.optimize(nIter);
-	printf("Optimizing for %d iterations\n",nIter);
+	struct timespec start,end;
+	clock_gettime(CLOCK_MONOTONIC,&start);
+	double RMSE = 0;
+	for (int i=0;i<numTrials;i++) {
+		//initialize solver
+		g2o::SparseOptimizer optimizer;
+		g2o::BlockSolverX::LinearSolverType *linearSolver = new g2o::LinearSolverDense<g2o::BlockSolverX::PoseMatrixType>();
+		g2o::BlockSolverX *blocksolver = new g2o::BlockSolverX(linearSolver);
+		g2o::OptimizationAlgorithmLevenberg* solver = new g2o::OptimizationAlgorithmLevenberg(blocksolver);
+		optimizer.setAlgorithm(solver);
 
-	//get result
-	printf("m: %f c:%f\n",v->estimate()(0),v->estimate()(1));
+		//add vertex
+		VertexLine *v = new VertexLine();
+		v->setId(0);
+		v->setEstimate(Eigen::Vector2d(0.0,2.0));
+		optimizer.addVertex(v);
+		//add edges
+		for (int i=0;i<num_edges;i++) {
+			double x = i + noise_sigma * randn();
+			double y = 2 * i + 3 + noise_sigma * randn();
+			EdgeLine* e = new EdgeLine();
+			e->setInformation(Eigen::Matrix<double,1,1>::Identity());
+			e->setVertex(0,v);
+			e->setMeasurement(Eigen::Vector2d(x,y));
+			optimizer.addEdge(e);
+		}
+
+		//optimize
+		int nIter = 10;
+		optimizer.initializeOptimization();
+		optimizer.setVerbose(false);
+		optimizer.optimize(nIter);
+		RMSE += (2 - v->estimate()(0)) * (2 - v->estimate()(0)) + (3 - v->estimate()(1)) * (3 - v->estimate()(1));
+
+	}
+	clock_gettime(CLOCK_MONOTONIC,&end);
+	double dt = end.tv_sec - start.tv_sec + 0.000000001 * (end.tv_nsec - start.tv_nsec);
+	RMSE = sqrt(RMSE / 2 / numTrials);
+	printf("Optimized %d trials sigma=%f (%fs,RMSE=%f)\n",numTrials,noise_sigma, dt / numTrials,RMSE);
 
 	return 0;
 }
